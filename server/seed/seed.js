3eaa135c2e2c7ed337b59c71493e7830d080b4e4
Aditya Gupta <adityag0537@gmail.com>
2026-06-04 10:54:44 +0530

    Removed seed questions and cleaned the database

--- server/seed/seed.js
+++ server/seed/seed.js
@@ -6,39 +6,15 @@ const connectDB = require("../config/db");
 const User = require("../models/User");
 const Question = require("../models/Question");
 const Answer = require("../models/Answer");
+const QuestionReport = require("../models/QuestionReport");
+const AnswerReport = require("../models/AnswerReport");
 const { ADMIN_EMAIL, ADMIN_PASSWORD } = require("../config/adminCredentials");
-const faqSeedData = require("./faqSeedData");
 
 dotenv.config();
 
-const seedDatabase = async ({ closeConnection = false } = {}) => {
-  if (mongoose.connection.readyState === 0) {
-    await connectDB();
-  }
+const SEED_USER_EMAIL = "seed@samagama.local";
 
-  const existingSeedCount = await Question.countDocuments({ isSeed: true });
-
-  if (existingSeedCount > 0) {
-    console.log("Seed FAQs already exist. Skipping seed.");
-    if (closeConnection) {
-      await mongoose.connection.close();
-    }
-    return;
-  }
-
-  const passwordHash = await bcrypt.hash(ADMIN_PASSWORD, 10);
-
-  let seedUser = await User.findOne({ email: "seed@samagama.local" });
-
-  if (!seedUser) {
-    seedUser = await User.create({
-      name: "Samagama Seed",
-      email: "seed@samagama.local",
-      passwordHash,
-      role: "USER",
-    });
-  }
-
+const ensureSeedAdmin = async (passwordHash) => {
   let adminUser = await User.findOne({ email: ADMIN_EMAIL });
 
   if (!adminUser) {
@@ -51,40 +27,101 @@ const seedDatabase = async ({ closeConnection = false } = {}) => {
     console.log(`Admin user created: ${ADMIN_EMAIL} / ${ADMIN_PASSWORD}`);
   }
 
-  const seedCreatedAt = new Date();
-  seedCreatedAt.setDate(seedCreatedAt.getDate() - 14);
+  return adminUser;
+};
 
-  for (const item of faqSeedData) {
-    const question = await Question.create({
-      title: item.title,
-      description: item.description,
-      categories: item.categories,
-      author: seedUser._id,
-      upvotes: item.upvotes,
-      views: item.views || 150,
-      isSeed: true,
-      createdAt: seedCreatedAt,
-      updatedAt: seedCreatedAt,
-    });
+const removeSeedQuestions = async () => {
+  const seedUser = await User.findOne({ email: SEED_USER_EMAIL }).select("_id");
+  const filters = [{ isSeed: true }];
 
-    const answer = await Answer.create({
-      questionId: question._id,
-      author: seedUser._id,
-      content: item.answer,
-      upvotes: 3,
-    });
-
-    question.acceptedAnswer = answer._id;
-    question.acceptedAnswerContent = item.answer;
-    question.acceptedAnswerSpAwardedTo = seedUser._id;
-    await question.save();
-
-    await User.findByIdAndUpdate(seedUser._id, {
-      $inc: { spPoints: 10 },
-    });
+  if (seedUser) {
+    filters.push({ author: seedUser._id });
   }
 
-  console.log(`Seeded ${faqSeedData.length} FAQ entries.`);
+  const seedQuestionIds = await Question.find({ $or: filters }).distinct("_id");
+
+  if (!seedQuestionIds.length) {
+    return 0;
+  }
+
+  await Answer.deleteMany({ questionId: { $in: seedQuestionIds } });
+  await Question.deleteMany({ _id: { $in: seedQuestionIds } });
+
+  return seedQuestionIds.length;
+};
+
+const removeNonAdminUsers = async () => {
+  const nonAdminUserIds = await User.find({ role: { $ne: "ADMIN" } }).distinct("_id");
+
+  if (!nonAdminUserIds.length) {
+    return {
+      users: 0,
+      questions: 0,
+      answers: 0,
+    };
+  }
+
+  const questionIds = await Question.find({
+    author: { $in: nonAdminUserIds },
+  }).distinct("_id");
+
+  const answerIds = await Answer.find({
+    $or: [
+      { author: { $in: nonAdminUserIds } },
+      { questionId: { $in: questionIds } },
+    ],
+  }).distinct("_id");
+
+  if (answerIds.length) {
+    await Question.updateMany(
+      { acceptedAnswer: { $in: answerIds } },
+      {
+        acceptedAnswer: null,
+        acceptedAnswerContent: "",
+        acceptedAnswerSpAwardedTo: null,
+      }
+    );
+    await AnswerReport.deleteMany({ answerId: { $in: answerIds } });
+  }
+
+  if (questionIds.length) {
+    await QuestionReport.deleteMany({ questionId: { $in: questionIds } });
+  }
+
+  await Answer.deleteMany({
+    $or: [
+      { _id: { $in: answerIds } },
+      { questionId: { $in: questionIds } },
+    ],
+  });
+  await Question.deleteMany({ _id: { $in: questionIds } });
+  await User.deleteMany({ _id: { $in: nonAdminUserIds } });
+
+  return {
+    users: nonAdminUserIds.length,
+    questions: questionIds.length,
+    answers: answerIds.length,
+  };
+};
+
+const seedDatabase = async ({ closeConnection = false } = {}) => {
+  if (mongoose.connection.readyState === 0) {
+    await connectDB();
+  }
+
+  const passwordHash = await bcrypt.hash(ADMIN_PASSWORD, 10);
+  await ensureSeedAdmin(passwordHash);
+
+  const removedCount = await removeSeedQuestions();
+  console.log(`Removed ${removedCount} seed question${removedCount === 1 ? "" : "s"}.`);
+
+  const nonAdminRemoved = await removeNonAdminUsers();
+  console.log(
+    `Removed ${nonAdminRemoved.users} non-admin user${nonAdminRemoved.users === 1 ? "" : "s"}, ` +
+      `${nonAdminRemoved.questions} question${nonAdminRemoved.questions === 1 ? "" : "s"}, ` +
+      `and ${nonAdminRemoved.answers} answer${nonAdminRemoved.answers === 1 ? "" : "s"}.`
+  );
+  console.log("Seed admin is available.");
 
   if (closeConnection) {
     await mongoose.connection.close();
